d80a468fb69bd7fd76c91f6cebe5c6424a982f37
Morevna <alwayslearn@mail.ru>
2025-10-03 23:01:59 +0300

    feat: add faq functionality

--- portfolio/script.js
+++ portfolio/script.js
@@ -13,7 +13,7 @@ links.forEach(link => {
   link.addEventListener('click', () => {
     burgerBtn.classList.remove('active');
     menu.classList.remove('open');
-        setTimeout(() => {
+    setTimeout(() => {
       html.classList.remove('no-scroll');
     }, 600);
   });
@@ -24,14 +24,78 @@ const slider = document.querySelector('.portfolio__slider');
 const leftZone = document.querySelector('.scroll-zone--left');
 const rightZone = document.querySelector('.scroll-zone--right');
 
-window.addEventListener('load', () => {
-  centerSlider();
+// window.addEventListener('load', () => {
+//   centerSlider();
+// });
+
+// function centerSlider() {
+//   const sliderWrapper = document.querySelector('.portfolio__slider-wrapper');
+//   const sliderWidth = slider.scrollWidth;
+//   const wrapperWidth = sliderWrapper.clientWidth;
+//   const centerScroll = (sliderWidth - wrapperWidth) / 2;
+//   slider.scrollLeft = centerScroll;
+// }
+
+
+// let scrollInterval;
+
+// function startScrolling(direction) {
+//   stopScrolling();
+//   scrollInterval = setInterval(() => {
+//     slider.scrollLeft += direction * 10;
+//   }, 16);
+// }
+
+// function stopScrolling() {
+//   clearInterval(scrollInterval);
+// }
+
+// leftZone.addEventListener('mouseenter', () => startScrolling(-1));
+// rightZone.addEventListener('mouseenter', () => startScrolling(1));
+// leftZone.addEventListener('mouseleave', stopScrolling);
+// rightZone.addEventListener('mouseleave', stopScrolling);
+
+
+
+
+///////////////////////////////////////////////////////////////
+const faqItems = document.querySelectorAll('.faq__item');
+
+faqItems.forEach(item => {
+  const question = item.querySelector('.faq__question');
+  const answer = item.querySelector('.faq__answer');
+  const toggle = item.querySelector('.faq__toggle');
+
+  question.addEventListener('click', () => {
+    const isOpen = answer.classList.contains('faq__answer--active');
+      //если вопрос открыт - закрываю
+    if (isOpen) {
+      answer.classList.remove('faq__answer--active');
+      toggle.textContent = "+";
+    } else {
+      // если был закрыт, то закрываю сначала все другие
+      faqItems.forEach(otherItem => {
+        otherItem.querySelector('.faq__answer').classList.remove('faq__answer--active');
+        otherItem.querySelector('.faq__toggle').textContent = "+";
+      });
+
+      // и потом открываю кликнутый вопрос
+      answer.classList.add('faq__answer--active');
+      toggle.textContent = "−";
+    }
+  });
 });
 
-function centerSlider() {
-  const sliderWrapper = document.querySelector('.portfolio__slider-wrapper');
-  const sliderWidth = slider.scrollWidth;
-  const wrapperWidth = sliderWrapper.clientWidth;
-  const centerScroll = (sliderWidth - wrapperWidth) / 2;
-  slider.scrollLeft = centerScroll;
-}
+
+
+// По умолчанию открыт первый аккордеон: +4
+//      просто в html оставить класс .faq__answer--active
+
+// Вы можете открыть или закрыть аккордеон, щелкнув в любом месте его заголовка: +4
+//         это область .faq__question
+
+// Одновременно может быть открыт только один аккордеон (открытие нового закрывает предыдущий): +8
+// проверка циклом кажого
+
+// Состояние аккордеона сохраняется после перезагрузки страницы: +8
+//    сохранить состояние в localStorage
