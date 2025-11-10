89d19a3323279d023fd20c0f2c732b7f0891d471
Morevna <alwayslearn@mail.ru>
2025-11-10 22:05:00 +0300

    fix: successfully set relative base path for GitHub pages

---
+++ pair-em-up/vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // Установка относительного пути для корректной работы на GitHub Pages
+  base: './', 
+   
+});
