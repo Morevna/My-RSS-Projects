4e7c86d1cd44d15d201af4fe00c3d0b5b225ff6b
Morevna <alwayslearn@mail.ru>
2025-12-04 10:20:27 +0300

    fix: add types and correct class structure in the file app.ts

---
+++ news-api/.eslintrc.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    "root": true,
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "project": [path.resolve(__dirname, "./tsconfig.json")],
+        "tsconfigRootDir": __dirname,
+        "ecmaVersion": 2020,
+        "sourceType": "module"
+    },
+    "plugins": ["@typescript-eslint", "prettier"],
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:prettier/recommended"
+    ],
+    "rules": {
+        "@typescript-eslint/no-explicit-any": "error",
+        "@typescript-eslint/strict-boolean-expressions": "error",
+        "prettier/prettier": "error"
+    },
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true
+    }
+};
--- news-api/src/components/app/app.ts
+++ news-api/src/components/app/app.ts
@@ -2,15 +2,17 @@ import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 
 class App {
-    constructor() {
-        private controller = new AppController();
-        private view = new AppView();
-    }
+    private controller = new AppController();
+    private view = new AppView();
+    constructor() {}
 
     start(): void {
-        document
-            .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
+        const sourcesElement = document.querySelector('.sources') as HTMLElement | null;
+        if (sourcesElement) {
+            sourcesElement.addEventListener('click', (e: MouseEvent) =>
+                this.controller.getNews(e, (data) => this.view.drawNews(data))
+            );
+        }
         this.controller.getSources((data) => this.view.drawSources(data));
     }
 }
